0c0f8e105241a6f35046b6d521e3fdd6b3afc617
Stéphane <stepdh01@gmail.com>
2023-01-03 19:27:36 +0100

    update modal for re-usable component

--- app-front/src/components/Modal.js
+++ app-front/src/components/Modal.js
@@ -1,42 +1,39 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
 const MODAL_STYLES = {
-    position: 'fixed',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    zIndex: 1050,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    outline: 0,
-  };
-  
-  const OVERLAY_STYLES = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 1040,
-    backgroundColor: 'rgba(0,0,0,.7)',
-  };
-  
+  position: "fixed",
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  zIndex: 1050,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  outline: 0,
+};
 
-const Modal = ( {open, children}) => {
-    if (!open) return null
-  return ReactDom.createPortal (
+const OVERLAY_STYLES = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 1040,
+  backgroundColor: "rgba(0,0,0,.7)",
+};
+
+const Modal = ({ open, children }) => {
+  if (!open) return null;
+  return ReactDom.createPortal(
     <>
-    <div style={OVERLAY_STYLES}>
-        <div style={MODAL_STYLES}>
-            {children}
-        </div>
-    </div>
+      <div style={OVERLAY_STYLES}>
+        <div style={MODAL_STYLES}>{children}</div>
+      </div>
     </>,
-    document.getElementById('portal')
-  )
-}
+    document.getElementById("portal")
+  );
+};
 
-export default Modal
+export default Modal;
