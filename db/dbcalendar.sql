96812fa566b7b8e627fd66405822bb030b613bf4
alexandreuy <uy.alexandre@yahoo.com>
2023-01-15 22:33:37 +0100

    cleaning

--- db/dbcalendar.sql
+++ db/dbcalendar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 12 jan. 2023 à 20:49
+-- Généré le : dim. 15 jan. 2023 à 21:33
 -- Version du serveur : 5.7.36
 -- Version de PHP : 7.4.26
 
@@ -29,23 +29,23 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `exam`;
 CREATE TABLE IF NOT EXISTS `exam` (
-  `idExam` int(11) NOT NULL AUTO_INCREMENT,
-  `startDate` datetime NOT NULL,
-  `duration` int(11) NOT NULL,
-  `Room_idRoom` int(11) NOT NULL,
-  `Subjects_idSubject` int(11) NOT NULL,
-  PRIMARY KEY (`idExam`),
-  KEY `fk_Exam_Room1_idx` (`Room_idRoom`),
-  KEY `fk_Exam_Subjects1_idx` (`Subjects_idSubject`)
+  `id_exam` int(11) NOT NULL AUTO_INCREMENT,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `room_id_room` int(11) NOT NULL,
+  `subjects_id_subjects` int(11) NOT NULL,
+  PRIMARY KEY (`id_exam`),
+  KEY `fk_Exam_Room1_idx` (`room_id_room`),
+  KEY `fk_Exam_Subjects1_idx` (`subjects_id_subjects`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `exam`
 --
 
-INSERT INTO `exam` (`idExam`, `startDate`, `duration`, `Room_idRoom`, `Subjects_idSubject`) VALUES
-(1, '2022-12-12 09:00:00', 2, 1, 1),
-(2, '2022-12-12 13:30:00', 3, 2, 5);
+INSERT INTO `exam` (`id_exam`, `start_date`, `end_date`, `room_id_room`, `subjects_id_subjects`) VALUES
+(1, '2022-12-12 09:00:00', '2022-12-12 10:00:00', 1, 1),
+(2, '2022-12-12 13:30:00', '2022-12-12 14:00:00', 2, 5);
 
 -- --------------------------------------------------------
 
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `hibernate_sequences` (
 --
 
 INSERT INTO `hibernate_sequences` (`sequence_name`, `next_val`) VALUES
-('default', 300);
+('default', 0);
 
 -- --------------------------------------------------------
 
@@ -121,7 +121,7 @@ CREATE TABLE IF NOT EXISTS `speciality` (
   `id_speciality` int(11) NOT NULL AUTO_INCREMENT,
   `name_speciality` varchar(45) NOT NULL,
   PRIMARY KEY (`id_speciality`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=454 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `speciality`
@@ -152,16 +152,26 @@ CREATE TABLE IF NOT EXISTS `speciality_has_subjects` (
 --
 
 INSERT INTO `speciality_has_subjects` (`speciality_id_speciality`, `subjects_id_subject`) VALUES
-(1, 1),
-(2, 1),
 (1, 2),
 (2, 2),
 (3, 2),
 (1, 3),
 (2, 3),
 (3, 3),
-(2, 4),
-(1, 5);
+(2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `user_id_user` int(11) NOT NULL,
+  `id_student` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -200,19 +210,31 @@ CREATE TABLE IF NOT EXISTS `subject` (
   `id_subject` int(11) NOT NULL AUTO_INCREMENT,
   `name_subject` varchar(45) NOT NULL,
   PRIMARY KEY (`id_subject`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=413 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `subject`
 --
 
 INSERT INTO `subject` (`id_subject`, `name_subject`) VALUES
-(1, 'Mathématique'),
+(1, 'Mathématiques'),
 (2, 'Francais'),
 (3, 'Histoire'),
 (4, 'SES'),
-(5, 'Physique'),
-(6, 'Littérature');
+(5, 'Physique');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `user_id_user` int(11) NOT NULL,
+  `id_teacher` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -228,14 +250,6 @@ CREATE TABLE IF NOT EXISTS `teacher_monitor_exam` (
   KEY `fk_User_has_Exam_Exam2_idx` (`Exam_idExam`),
   KEY `fk_User_has_Exam_User2_idx` (`User_id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `teacher_monitor_exam`
---
-
-INSERT INTO `teacher_monitor_exam` (`User_id_user`, `Exam_idExam`) VALUES
-(1, 1),
-(2, 2);
 
 -- --------------------------------------------------------
 
@@ -253,44 +267,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_user`),
   KEY `fk_role_idx` (`role`)
-) ENGINE=InnoDB AUTO_INCREMENT=203 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=362 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`id_user`, `firstname`, `lastname`, `email`, `password`, `role`) VALUES
-(1, 'fsdqfsq', 'fsdqfsqd', 'dsfsqfq', 'fdsfsd', 1),
+(1, 'Graci', 'TAPEUR', 'graci@tapeur.fr', 'gracita', 1),
 (2, 'Alex', 'Sansdrap', 'alex@sansdrap.fr', 'alexandre', 1),
 (3, 'Steph', 'anatik', 'Steph@anatik.fr', 'stephane', 1),
 (4, 'Jay-B', 'sy', 'jay-b@sy.fr', 'jb', 1),
-(5, 'sebas', 'tient', 'sebas@tien.fr', 'sebastien', 1),
-(6, 'infor', 'matic', 'infor@matic.fr', 'informatique', 1),
-(7, 'Napoleon', 'bonappart', 'napoleonbonapart.fr', 'napoleon', 1),
-(8, 'Emmanuel', 'macaron', 'emmanuel@macaron.fr', 'emmanuel', 1),
 (9, 'isaac', 'newton', 'isaac@newton.fr', 'isaac', 1),
-(10, 'lavoi', 'zier', 'lavoi@zier.fr', 'lavoisier', 1),
-(11, 'peeta', 'Gore', 'peeta@gore.fr', 'pythagore', 1),
-(152, 'fsdqfsq', 'fsdqfsqd', 'dsfsqfq', 'fdsfsd', 1),
-(202, 'fsdqfsq', 'fsdqfsqd', 'dsfsqfq', 'fdsfsd', 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user_seq`
---
-
-DROP TABLE IF EXISTS `user_seq`;
-CREATE TABLE IF NOT EXISTS `user_seq` (
-  `next_val` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `user_seq`
---
-
-INSERT INTO `user_seq` (`next_val`) VALUES
-(101);
+(359, 'caca', 'fqsfdq', 'fsdqfsq', 'fqdqsfq', 1),
+(360, 'Alexandreggfsd', 'UYgdsfgdsfdsqfqs', 'alexandre@gmail.com', 'fdsqfdsq', 2),
+(361, 'fsqdfqs', 'fqsfsq', 'fdqsfsq', 'fdqfsq', 1);
 
 --
 -- Contraintes pour les tables déchargées
@@ -300,8 +291,8 @@ INSERT INTO `user_seq` (`next_val`) VALUES
 -- Contraintes pour la table `exam`
 --
 ALTER TABLE `exam`
-  ADD CONSTRAINT `fk_Exam_Room1` FOREIGN KEY (`Room_idRoom`) REFERENCES `room` (`id_room`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Exam_Subjects1` FOREIGN KEY (`Subjects_idSubject`) REFERENCES `subject` (`id_subject`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Exam_Room1` FOREIGN KEY (`room_id_room`) REFERENCES `room` (`id_room`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Exam_Subjects1` FOREIGN KEY (`subjects_id_subjects`) REFERENCES `subject` (`id_subject`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `speciality_has_subjects`
@@ -314,14 +305,14 @@ ALTER TABLE `speciality_has_subjects`
 -- Contraintes pour la table `student_has_exam`
 --
 ALTER TABLE `student_has_exam`
-  ADD CONSTRAINT `fk_User_has_Exam_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_User_has_Exam_Exam1` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`id_exam`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_User_has_Exam_User1` FOREIGN KEY (`User_id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `teacher_monitor_exam`
 --
 ALTER TABLE `teacher_monitor_exam`
-  ADD CONSTRAINT `fk_User_has_Exam_Exam2` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`idExam`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_User_has_Exam_Exam2` FOREIGN KEY (`Exam_idExam`) REFERENCES `exam` (`id_exam`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_User_has_Exam_User2` FOREIGN KEY (`User_id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
